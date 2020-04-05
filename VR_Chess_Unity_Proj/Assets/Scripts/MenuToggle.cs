cea426df2ab1a51bec6437a8b70290ef20a5a89a
mrock724 <mrock724@gmail.com>
2020-04-05 18:19:01 -0400

    Added the blender models
    Added Sebastian's chess piece models to the scene to replace the placeholder models.
    Also minorly adjusted the height of the table.

---
+++ VR_Chess_Unity_Proj/Assets/Scripts/MenuToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MenuToggle : MonoBehaviour
+{
+    public VRTK_ControllerEvents controllerEvents;
+    public GameObject menu;
+    bool menuState = false;
+
+    private void OnEnable()
+    {
+        controllerEvents.ButtonTwoReleased += ControllerEvents_ButtonTwoReleased;
+    }
+
+    private void OnDisable()
+    {
+        controllerEvents.ButtonTwoReleased -= ControllerEvents_ButtonTwoReleased;
+    }
+
+    private void ControllerEvents_ButtonTwoReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        menuState = !menuState;
+        menu.SetActive(menuState);
+    }
+}
